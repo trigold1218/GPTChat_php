86333abfe69d6a3be52fa882a86f4d265932df1f
trigold <422520625@qq.com>
2022-12-09 15:05:57 +0800

    init

--- src/GPTChat.php
+++ src/GPTChat.php
@@ -2,26 +2,78 @@
 
 namespace Trigold\GptChat;
 
+use Exception;
 use GuzzleHttp\Client;
 use Illuminate\Support\Str;
 use GuzzleHttp\Cookie\CookieJar;
+use GuzzleHttp\Exception\GuzzleException;
 
 class GPTChat
 {
+    /**
+     * auth key name
+     * @var string
+     */
     const AUTH_COOKIE_NAME = "__Secure-next-auth.session-token";
+
+    /**
+     * base url
+     * @var string
+     */
     const BASE_URL = 'https://chat.openai.com';
+
+    /**
+     * auth url
+     * @var string
+     */
     const AUTH_URI = '/api/auth/session';
+
+    /**
+     * conversation url
+     * @var string
+     */
     const CONV_URL = '/backend-api/conversation';
+
+    /**
+     * default user agent
+     * @var array
+     */
     const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36';
 
-    protected Client $client;
-    protected string $sessionToken;
-    protected string $parent_message_id;
-    protected bool $authFlag = false;
     /**
+     * GuzzleHttp\Client
+     * @var Client
+     */
+    protected Client $client;
+
+    /**
+     * user session token
+     * @var string
+     */
+    protected string $sessionToken;
+
+    /**
+     * parent message id
+     * @var string
+     */
+    protected string $parent_message_id;
+
+    /**
+     * Whether to obtain authorization
+     * @var bool
+     */
+    protected bool $authFlag = false;
+
+    /**
+     * request token
      * @var string
      */
     protected string $accessToken;
+
+    /**
+     * conversation id
+     * @var string
+     */
     protected string $conversation_id;
 
     public function __construct($sessionToken)
@@ -33,6 +85,11 @@ class GPTChat
         $this->parent_message_id = Str::orderedUuid()->toString();
     }
 
+    /**
+     * get auth token
+     * @return $this
+     * @throws GuzzleException
+     */
     public function authenticate(): GPTChat
     {
         $this->accessToken = $this->getAccessToken();
@@ -40,11 +97,18 @@ class GPTChat
         return $this;
     }
 
-
+    /**
+     * send message
+     *
+     * @param $msg
+     *
+     * @return void
+     * @throws Exception|GuzzleException
+     */
     public function send_message($msg)
     {
         if (!$this->authFlag) {
-            throw new \Exception("In order to send messages you have to authenticate first.");
+            throw new Exception("In order to send messages you have to authenticate first.");
         }
 
         $data = $this->buildMessage($msg, Str::orderedUuid()->toString(), $this->conversation_id,
@@ -57,7 +121,7 @@ class GPTChat
         ]);
         $code = $response->getStatusCode();
         if ($code !== 200) {
-            throw new \Exception("Authentication failed with code: {$code}");
+            throw new Exception("Authentication failed with code: {$code}");
         }
 
         $body = $response->getBody();
@@ -67,7 +131,10 @@ class GPTChat
         }
     }
 
-
+    /**
+     * get cookieJar
+     * @return CookieJar
+     */
     protected function getAuthCookie(): CookieJar
     {
         return CookieJar::fromArray([
@@ -75,6 +142,11 @@ class GPTChat
         ], self::BASE_URL);
     }
 
+    /**
+     * get access token
+     * @return mixed
+     * @throws GuzzleException
+     */
     protected function getAccessToken()
     {
         $response = $this->client->get(self::AUTH_URI, [
@@ -93,7 +165,15 @@ class GPTChat
         return $result['accessToken'];
     }
 
-
+    /**
+     * build message
+     * @param $message
+     * @param $id
+     * @param $conv_id
+     * @param $parent_msg_id
+     *
+     * @return array
+     */
     public function buildMessage($message, $id, $conv_id, $parent_msg_id): array
     {
         $data = [
@@ -118,6 +198,10 @@ class GPTChat
         return $data;
     }
 
+    /**
+     * get conversation headers
+     * @return string[]
+     */
     protected function convHeaders(): array
     {
         return [
@@ -126,5 +210,27 @@ class GPTChat
             'Content-Type'  => 'application/json',
             'User-Agent'    => self::DEFAULT_USER_AGENT,
         ];
+    }
+
+    /**
+     * set conversation id
+     * @param $conversation_id
+     *
+     * @return $this
+     */
+    public function setConversationId($conversation_id): GPTChat
+    {
+        $this->conversation_id = $conversation_id;
+        return $this;
+    }
+
+    /**
+     * if you think that the conversation is over, you can reset the conversation id
+     * @return $this
+     */
+    public function clearConversationId(): GPTChat
+    {
+        $this->conversation_id = null;
+        return $this;
     }
 }
